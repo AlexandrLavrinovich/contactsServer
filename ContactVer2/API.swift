978e23502cf4287415b557d494f7e647c121e0d5
Dinolavr <Dinolavr@users.noreply.github.com>
2019-11-19 03:04:18 +0500

    withoudLoad

--- ContactVer2/API.swift
+++ ContactVer2/API.swift
@@ -7,31 +7,31 @@ enum API {
     static var identifier: String { "lavr" }
     static var baseURL: String { "https://ios-napoleonit.firebaseio.com/data/\(identifier)/" }
     
-    static func loadNotes(completion: @escaping ([Contacts]) -> Void) {
-        let url = URL(string: baseURL + ".json")!
-        let request = URLRequest(url: url)
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard
-                let data = data,
-                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? JSON
-                else { return }
-
-            let notesJSON = json["notes"] as! JSON
-            var notes = [Contacts]()
-
-            for note in notesJSON {
-                notes.append(Contacts(data: note.value as! JSON))
-            }
-
-//            notes.sort { $0.date < $1.date }
-
-            DispatchQueue.main.async {
-                completion(notes)
-            }
-        }
-        task.resume()
-    }
+//    static func loadNotes(completion: @escaping ([Contacts]) -> Void) {
+//        let url = URL(string: baseURL + ".json")!
+//        let request = URLRequest(url: url)
+//
+//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//            guard
+//                let data = data,
+//                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? JSON
+//                else { return }
+//
+//            let notesJSON = json["notes"] as! JSON
+//            var notes = [Contacts]()
+//
+//            for note in notesJSON {
+//                notes.append(Contacts(data: note.value as! JSON))
+//            }
+//
+////            notes.sort { $0.date < $1.date }
+//
+//            DispatchQueue.main.async {
+//                completion(notes)
+//            }
+//        }
+//        task.resume()
+//    }
     
     static func createContact(name: String, phoneNumber: String,contactId: String, completion: @escaping (Bool) -> Void) {
         let params = [
